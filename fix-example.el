ac71d8f799c90219716782153bdf90f386d1ffca
Phil Beadling <phil.beadling@opetek.io>
2024-01-14 17:15:54 +0000

    update path

--- fix-example.el
+++ fix-example.el
@@ -1,4 +1,4 @@
-(module-load "/home/phil/git/quickfix_emacs/emacs-fix-parser.so")
+(module-load "/home/phil/git/emacs-fix-message-parser/emacs-fix-parser.so")
 
 (message "Parsed FIX message: %s" (parse-fix-message "8=FIX.4.49=14835=D34=108049=TESTBUY152=20180920-18:14:19.50856=TESTSELL111=63673064027889863415=USD21=238=700040=154=155=MSFT60=20180920-18:14:19.49210=092"))
 
